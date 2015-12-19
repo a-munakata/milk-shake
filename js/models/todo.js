0e416a19b5c0acf10c43aeffb68f3680a0eb4f27
a-munakata <a.munakata@4digit.jp>
2015-12-19 11:20:02 +0900

    Refactored Todo.js

--- js/models/todo.js
+++ js/models/todo.js
@@ -1,28 +1,36 @@
-App.Todo = function(){};
-
-App.Todo.query = function(props){
-  return $.ajax({
-    type: "GET",
-    url: App.ENDPOINT + "/api/v1/projects/" + props.project_id + "/todos"
-  });
-};
-
-App.Todo.create = function(props){
-  return $.ajax({
-    type: "POST",
-    url: App.ENDPOINT + "/api/v1/projects/" + props.project_id + "/todos",
-    data: props
-  });
-};
-
-App.Todo.update = function(props){
-  return $.ajax({
-    type: "PUT",
-    url: App.ENDPOINT + "/api/v1/todos/" + props.id,
-    data: props
-  });
-};
-
-App.Todo.prototype = {
-  constructor: App.Todo
+App.Todo = function(){
+  return {
+    query: function(props){
+      return $.ajax({
+        type: 'GET',
+        url: App.Endpoint + '/api/v1/projects/' + props.project_id + '/todos'
+      });
+    },
+    show: function(props){
+      /*
+       unused
+       */
+    },
+    create: function(props){
+      return $.ajax({
+        type: 'POST',
+        url: App.Endpoint + '/api/v1/projects/' + props.project_id + '/todos',
+        data: props
+      });
+    },
+    update: function(props){
+      return $.ajax({
+        type: 'PUT',
+        url: App.Endpoint + '/api/v1/todos/' + props.id,
+        data: props
+      });
+    },
+    destroy: function(props){
+      return $.ajax({
+        type: 'DELETE',
+        url: App.Endpoint + '/api/v1/todos/' + props.id,
+        data: props
+      });
+    }
+  };
 };
